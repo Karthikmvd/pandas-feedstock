54fa2798bb8ab8e21fb85daca4adfc272a0eef24
KARTHIK M <makkthic@in.ibm.com>
2018-05-08 13:05:16 +0530

    Update run_docker_build.sh

--- .circleci/run_docker_build.sh
+++ .circleci/run_docker_build.sh
@@ -6,4 +6,4 @@
 # benefit from the improvement.
 export PATH=$PATH:/root/anaconda3/bin/conda
 echo $?
-/root/anaconda3/bin/conda build /home/conda/recipe_root --quiet || exit 1
+sudo /root/anaconda3/bin/conda build /home/conda/recipe_root --quiet || exit 1
